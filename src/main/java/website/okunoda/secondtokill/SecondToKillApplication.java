845957aec0a2f0a4f6ae16bdedac77f9bc1d1112
Okunoda <south_song@163.com>
2023-03-11 14:30:04 +0800

    首次提交

---
+++ src/main/java/website/okunoda/secondtokill/SecondToKillApplication.java
@@ -0,0 +1,13 @@
+package website.okunoda.secondtokill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondToKillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondToKillApplication.class, args);
+    }
+
+}
